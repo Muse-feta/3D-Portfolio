08bce463ac2f4aefa3f6ae2a4a1db2fb58cff8e1
Muse-feta <fetamuse@gmail.com>
2024-12-02 20:23:50 +0300

    first commit

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import "./globals.css";
+
+
+
+export const metadata = {
+  title: "Muse | Portfolio",
+  description:
+    "Muse is a creative Full Stack Web Developer based in Ethiopia. Passionate and dedicated to the craft, Muse brings a few years of professional experience as a Full Stack Developer. Equipped with the skills to create great, appealing, and premium websites, Muse ensures every project aligns with the latest web standards.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={``}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
