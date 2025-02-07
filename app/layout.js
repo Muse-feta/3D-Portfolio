895f3ba83a92fa73bd1d38cda3ba883033c8617c
Muse-Feta <133772190+Muse-feta@users.noreply.github.com>
2025-02-07 14:42:06 +0300

    Added Google Search Console verification

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,9 @@ export const metadata = {
   title: "Muse | Portfolio",
   description:
     "Muse is a creative Full Stack Web Developer based in Ethiopia. Passionate and dedicated to the craft, Muse brings a few years of professional experience as a Full Stack Developer. Equipped with the skills to create great, appealing, and premium websites, Muse ensures every project aligns with the latest web standards.",
+  verification: {
+    google: "XUp4JdVHCCfCCdsNO19XllztF0mlUSaAn5_VjbOayHw",
+  },
 };
 
 export default function RootLayout({ children }) {
