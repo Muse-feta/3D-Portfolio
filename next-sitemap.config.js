9f38d5d25d7bb74d39efc6da5cd58e1772f7972a
Muse-feta <fetamuse@gmail.com>
2025-02-07 15:18:46 +0300

    Ensure sitemap.xml is generated on Vercel

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://musefeta.vercel.app", // Change this to your actual domain if needed
+  generateRobotsTxt: true, // Generates a robots.txt file
+};
