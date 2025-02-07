22822f4d1e4e9e241edc995510a54c9f8ea619e8
Muse-Feta <133772190+Muse-feta@users.noreply.github.com>
2025-02-08 00:01:05 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./index.css";
 
 
 export const metadata = {
-  title: "Muse | Portfolio",
+  title: "Muse Feta | Portfolio",
   description:
     "Muse is a creative Full Stack Web Developer based in Ethiopia. Passionate and dedicated to the craft, Muse brings a few years of professional experience as a Full Stack Developer. Equipped with the skills to create great, appealing, and premium websites, Muse ensures every project aligns with the latest web standards.",
   verification: {
