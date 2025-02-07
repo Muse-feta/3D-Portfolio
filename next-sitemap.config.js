5ff6f3f39bd84bb1a035a8c7956f4958b4cfcc79
Muse-feta <fetamuse@gmail.com>
2025-02-07 15:58:54 +0300

    modified my sitemap file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,4 +2,6 @@
 module.exports = {
   siteUrl: "https://musefeta.vercel.app", // Change this to your actual domain if needed
   generateRobotsTxt: true, // Generates a robots.txt file
+  sitemapSize: 5000, // Ensures all URLs are in one file
+  generateIndexSitemap: false, // Prevents splitting into sitemap-0.xml
 };
